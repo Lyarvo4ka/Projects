a936111ec53a1233833dc9af6b7ae437aec2e58f
Lyarvo4ka <Lyarvo4ka@gmail.com>
2016-01-30 15:07:28 +0200

    IOdevice interface

--- include/IOLibrary/IODevice.h
+++ include/IOLibrary/IODevice.h
@@ -11,20 +11,36 @@ namespace IO
 	class IODevice
 	{
 	public:
-		virtual bool open(const std::wstring & , OpenMode) = 0;
+		virtual bool open(OpenMode) = 0;
+		virtual void close() = 0;
 		virtual bool is_open() = 0;
 		virtual void set_position(uint64_t offset) = 0;
 		virtual uint32_t read_data(uint8_t * data, uint32_t read_size) = 0;
+		virtual uint32_t write_data(uint8_t * data, uint32_t read_size) = 0;
+		virtual uint64_t size() const = 0;
 	};
 
-	class FileDevice
+	class File
 		: public IODevice
 	{
 	private:
 		HANDLE hFile_;
 		uint64_t offset_;
+		uint64_t size_;
+		std::wstring file_name_;
+		bool bOpen_;
 	public:
-		bool open(const std::wstring & file_name, OpenMode openMode) override
+		File(const std::wstring & file_name)
+			: hFile_(INVALID_HANDLE_VALUE)
+			, offset_( 0 )
+			, size_(0)
+			, file_name_(file_name)
+			, bOpen_(false)
+		{
+
+		}
+
+		bool open( OpenMode openMode) override
 		{
 			uint32_t win_open_mode = 0;
 
@@ -37,7 +53,7 @@ namespace IO
 				win_open_mode = CREATE_ALWAYS;
 			}
 
-			::CreateFile( file_name.c_str(), 
+			::CreateFile( file_name_.c_str(), 
 						  GENERIC_READ | GENERIC_WRITE,
 						  FILE_SHARE_READ | FILE_SHARE_WRITE,
 						  NULL,
@@ -45,6 +61,7 @@ namespace IO
 						  0,
 						  NULL);
 		}
+
 		void set_position(uint64_t offset) override 
 		{
 			offset_ = offset;
@@ -59,5 +76,49 @@ namespace IO
 			//if ( !::ReadFile(hFile_ , buffer , read_size , &bytes_read) )
 		};
 
+	};
+
+	class PysicalDrive
+	{
+	private:
+		uint32_t drive_number_;
+		uint64_t number_sectors_;
+		uint32_t bytes_per_sector_;
+//		std::wstring path_;
+	public:
+		//PysicalDrive(const std::string path)
+		//	: drive_number_(0)
+		//	, number_sectors_(0)
+		//	, bytes_per_sector_(0)
+		//	, path_(path)
+		//{
+
+		//}
+
+
+		void setDriveNumber(uint32_t drive_number)
+		{
+			drive_number_ = drive_number;
+		}
+		uint32_t getDriveNumber() const
+		{
+			return drive_number_;
+		}
+		void setNumberSectors(uint64_t number_sectors)
+		{
+			number_sectors_ = number_sectors;
+		}
+		uint64_t getNumberSectors() const
+		{
+			return number_sectors_;
+		}
+		void setBytesPerSector(uint32_t bytes_per_sector)
+		{
+			bytes_per_sector_ = bytes_per_sector;
+		}
+		uint32_t getBytesPerSector() const
+		{
+			return bytes_per_sector_;
+		}
 	};
 }
